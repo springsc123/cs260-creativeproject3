5cf27b55e3a0d49ae5e4cf2016c92983cce6c9ff
springsc123 <springscullen@yahoo.com>
2019-10-25 15:48:25 +0000

    init

---
+++ script.js
@@ -0,0 +1,124 @@
+Vue.component('star-rating', VueStarRating.default);
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        number: '',
+        max: '',
+        current: {
+            title: '',
+            img: '',
+            alt: ''
+        },
+        loading: true,
+        loading: true,
+        addedName: '',
+        addedComment: '',
+        comments: [],
+        ratings: []
+    },
+    created() {
+        this.xkcd();
+    },
+    methods: {
+        async xkcd() {
+            try {
+                this.loading = true;
+                const response = await axios.get('https://xkcdapi.now.sh/' + this.number);
+                this.current = response.data;
+                this.loading = false;
+                this.number = response.data.num;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        },
+        previousComic() {
+            this.number = this.current.num - 1;
+            if (this.number < 1)
+                this.number = 1;
+        },
+        nextComic() {
+            this.number = this.current.num + 1;
+            if (this.number > this.max)
+                this.number = this.max
+        },
+        firstComic() {
+            this.number = this.max;
+        },
+        lastComic() {
+            this.number = 1;
+        },
+        getRandom(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum and minimum are inclusive
+        },
+        randomComic() {
+            this.number = this.getRandom(1, this.max);
+        },
+        addComment() {
+            if (!(this.number in this.comments))
+                Vue.set(app.comments, this.number, new Array);
+            this.comments[this.number].push({
+                author: this.addedName,
+                text: this.addedComment,
+                time: this.time
+            });
+            this.addedName = '';
+            this.addedComment = '';
+        },
+        setRating(rating) {
+            if (!(this.number in this.ratings))
+                Vue.set(this.ratings, this.number, {
+                    sum: 0,
+                    total: 0
+                });
+            this.ratings[this.number].sum += rating;
+            this.ratings[this.number].total += 1;
+        }
+
+    },
+    computed: {
+        month() {
+            var month = new Array;
+            if (this.current.month === undefined)
+                return '';
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "November";
+            month[11] = "December";
+            return month[this.current.month - 1];
+        },
+        time() {
+            return moment().format('MMMM Do YYYY, h:mm:ss a');
+        },
+        avg() {
+            if (this.number in this.ratings) {
+
+                return this.ratings[this.number].sum / this.ratings[this.number].total
+            }
+            else {
+                return 0;
+            }
+        }
+    },
+    watch: {
+        number(value, oldvalue) {
+            if (oldvalue === '') {
+                this.max = value;
+            }
+            else {
+                this.xkcd();
+            }
+        },
+    },
+});
